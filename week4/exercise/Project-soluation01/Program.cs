17ea49f203ca7c0df5b69d78876a2d9a2b425292
“karim <moza.m@gmail.com>
2026-05-11 11:31:55 +0400

    async

---
+++ week4/exercise/Project-soluation01/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Project_soluation01
+{
+    #region async
+
+    
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello before method");
+            string num = await GetNumber();
+            Console.WriteLine(num);
+            Console.WriteLine("Hello after method");
+        }
+
+        static async Task<string> GetNumber()
+        {
+            await Task.Delay(2000);
+            return "Hello from async method";
+
+#endregion
+        }
+    }
+}
