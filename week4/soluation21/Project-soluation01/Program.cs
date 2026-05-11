4bfb03811ad473e4d6050bbf0d543b73ff09e515
“karim <moza.m@gmail.com>
2026-05-11 08:10:32 +0400

    add file

---
+++ week4/soluation21/Project-soluation01/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Project_soluation01
+{
+    internal class Program
+    {
+        #region interface
+
+        
+        interface bird
+        {
+            string Name { get; }
+            public void Fly();
+        }
+        class hodhod : bird
+        {
+            public string Name { get; set; }
+            public void Fly()
+            {
+                Console.WriteLine("Hello");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+#endregion
+
+
+        }
+    }
+}
