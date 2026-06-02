86ab5d8146be699e9bd780c30990e3c47acbc242
moza-stack <moza.m@gmail.com>
2026-06-02 10:46:26 +0400

    add file

---
+++ week4/Soluation50/Project/Guest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    internal class Guest
+    {
+        private static int totalGuestsCreated;
+        private string nationalID;
+        private string fullName;
+        public string NationalID
+        {
+            get;
+        }
+        public string FullName
+        {
+            get; set;
+        }
+        public string GuestfullName;
+        public string GuestnationalID;
+        public static int GetTotalGuestsCreated()
+        {  return totalGuestsCreated; }
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Guest Name: {fullName}");
+            Console.WriteLine($"National ID: {nationalID}");
+        }
+                
+        }
+
+
+
+
+    }
+        
+    
+
